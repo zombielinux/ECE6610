35f7d01117e7c3a32117b9d6f66782500bc2d98f
zombielinux <will@sutton-family.org>
2017-09-23 09:37:04 -0400

    Added data and results.txt

--- Assignment2/homework.sh
+++ Assignment2/homework.sh
@@ -4,10 +4,13 @@ list="TcpNewReno TcpHybla TcpHighSpeed TcpHtcp TcpVegas TcpScalable TcpVeno TcpB
 
 for j in $list
 	do
-	for i in 1 4 16 32
+	for i in 1 4 8 16 32
 		do
 		cd /home/will/NS3/ns-allinone-3.26/ns-3.26/scratch/
 		/home/will/NS3/ns-allinone-3.26/ns-3.26/waf --run "p1 --nSpokes=$i --Protocol=$j" > /home/will/Homework/Assignment2/$j$i.txt
 		sed -i -e 1,4d /home/will/Homework/Assignment2/$j$i.txt
+		totaltx="$(cat /home/will/Homework/Assignment2/$j$i.txt | grep Totals | cut -c 17-23)"
+		echo "${j},${i},${totaltx}" >> /home/will/Homework/Assignment2/results.txt
 		done
 	done
+# | grep Throughput | cut -c 17-23
