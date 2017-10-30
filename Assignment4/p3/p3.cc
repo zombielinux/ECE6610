dc9ac625b0ae0ac9a534bdadf9dc5bd171f13beb
zombielinux <will@sutton-family.org>
2017-10-30 14:43:43 -0400

    started

--- Assignment4/p3/p3.cc
+++ Assignment4/p3/p3.cc
@@ -8,15 +8,15 @@
 #include "p2pCampusHelper.h"
 
 #include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/netanim-module.h"
-#include "ns3/internet-apps-module.h"
-#include "ns3/traffic-control-module.h"
-#include "ns3/point-to-point-layout-module.h"
-#include "ns3/flow-monitor-module.h"
+//#include "ns3/network-module.h"
+//#include "ns3/internet-module.h"
+//#include "ns3/point-to-point-module.h"
+//#include "ns3/applications-module.h"
+//#include "ns3/netanim-module.h"
+//#include "ns3/internet-apps-module.h"
+//#include "ns3/traffic-control-module.h"
+//#include "ns3/point-to-point-layout-module.h"
+//#include "ns3/flow-monitor-module.h"
 
 
 int main(int argc, char* argv[]) {
