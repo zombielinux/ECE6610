fd8ef353bfc6e99aa2c52ec222cce19e13a02ac8
zombielinux <will@sutton-family.org>
2017-10-30 14:23:49 -0400

    started

--- Assignment4/p3/p3.cc
+++ Assignment4/p3/p3.cc
@@ -11,4 +11,13 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "Hello" << std::endl; 
 
+  rc = MPI_Init(&argc,&argv);
+  if (rc != MPI_SUCCESS) {
+    std::cout << "Error starting MPI program. Terminating." << std:endl;
+ 		MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  if (rc == MPI_SUCCESS) {
+    std::cout << "MPI! It VERKS"" << std:endl;
+  }
+
 }
